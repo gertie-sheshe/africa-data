1e23caf794c808f02252a8a03bc7a1efb0dd0b74
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-06 15:37:58 +0300

    make it responsive

--- src/components/maincontent/main-content.js
+++ src/components/maincontent/main-content.js
@@ -86,37 +86,18 @@ const dataFour = {
     ]
 };
 
-const dataFive = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
-
-
   
 const MainContent = () => (
     <div className="section-2">
         <div className="details">Capitale Popuation Size </div>
         <div className="upper-section">
             <div className="charts">
-                <div>
+                <div className="chart-display">
                     <Pie data={data}/>
                 </div>
-                <div>
+                <div className="chart-display">
                     <Pie data={dataTwo}/>
                 </div>
-                {/* <div>
-                    <Pie data={data}/>
-                </div> */}
             </div>
             <div className="line-upper">
                 <div>
